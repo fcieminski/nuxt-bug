7ea013a23e18b0e8adbb631d7494c57a013d6da5
Filip <f.cieminski@gmail.com>
2024-06-30 13:13:21 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+<NuxtLink to="/test">Test page</NuxtLink>
+<NuxtLink to="/test-second">Test page second</NuxtLink>
+  </div>
+  </template>
---
+++ pages/test-second.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    test page second
+  </div>
+  </template>
